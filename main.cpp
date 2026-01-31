241525e734ebcbb95d8947117f01f1358561550d
Mario Fonseca <mario.fonseca@oracle.com>
2026-01-31 01:21:59 -0600

    Adding comments in code

--- main.cpp
+++ main.cpp
@@ -10,9 +10,12 @@ void imprimeArreglo(float *d);
 int main()
 {
     std::cout << "Sumando arreglos en paralelo \n";
+
+    // Creando arreglos
     float a[N], b[N], c[N];
     int i;
     
+    // Generación de numeros para asignar a los arreglos
     for(i = 0; i < N; i++){
         a[i] = i * 10;
         b[i] = (i + 3) * 2 + 8 * 5;
@@ -20,14 +23,17 @@ int main()
     
     int pedazos = chunk;
     
+    // Usando OpenMP para usar programación en paralelo
     #pragma omp parallel for \
     shared(a, b, c, pedazos) private(i) \
     schedule(static, pedazos)
     
+    // Haciendo la suma
     for (i = 0; i < N; i++){
         c[i] = a[i] + b[i];
     }
     
+    // Imprimiendo arreglos
     std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << std::endl;
     imprimeArreglo(a);
     std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << std::endl;
@@ -39,6 +45,7 @@ int main()
     
 }
 
+// Método auxiliar para imprimir arreglos
 void imprimeArreglo(float *d){
     for(int i = 0; i < mostrar; i++){
         std::cout << d[i] << " - ";
